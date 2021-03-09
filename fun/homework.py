efb927dd9710d20bc2a35c4d68a8fb799cbe8fcf
vcu-gooww <78395107+vcu-gooww@users.noreply.github.com>
2021-03-08 23:15:47 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,9 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-   largest_value = max(incoming_list)
-   return largest_value
-   
+    return max(incoming_list)
 
 
 def find_least_number(incoming_list):
@@ -16,8 +14,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    smallest_value = min(incoming_list)
-    return smallest_value
+    return min(incoming_list)
 
 
 def add_list_numbers(incoming_list):
@@ -25,12 +22,11 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
+
     if incoming_list is None:
         return 0
-    total_value = sum(incoming_list)
-    return total_value
-        
-
+    return_value = sum(incoming_list)
+    return return_value
 
 
 def longest_value_key(incoming_dict):
@@ -38,9 +34,8 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    if incoming_dict is not None:
-        if len(incoming_dict) == 0:
-            return None
-        final_key = max(incoming_dict, key=lambda k: len(incoming_dict[k]))
-        return fianal_key
-    return None
+    try:
+        return max(incoming_dict, key=lambda x: len(incoming_dict[x]))
+    except:
+        incoming_dict = None
+        return None
