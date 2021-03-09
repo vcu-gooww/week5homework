b1ff426245925c07371d7c1edcef690a17239865
vcu-gooww <78395107+vcu-gooww@users.noreply.github.com>
2021-03-08 22:27:46 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,9 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    pass
+    greatest_value = max(incoming_list)
+    return greatest_value
+   
 
 
 def find_least_number(incoming_list):
@@ -14,16 +16,20 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    pass
-
+    smallest_value = min(incoming_list)
+    return smallest_value
 
 def add_list_numbers(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
+    if incoming_list is None:
+        return 0
+    return_value = sum(incoming_list)
+    return return_value
+        
 
-    pass
 
 
 def longest_value_key(incoming_dict):
